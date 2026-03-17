58ee48cea5281e1dd45cc3bf2703ce0cd80343e7
humbertomaia161-gif <humbertomaia161@gmail.com>
2026-03-17 16:32:58 -0300

    meu primeiro commit

---
+++ main.js
@@ -0,0 +1,20 @@
+alert("Ola mundo");
+
+const numeroAleatorio = 10 
+let chute = prompt("Mini game número secreto!!!")
+let tentativas = 1;
+console.log(numeroAleatorio);
+
+while (chute != numeroAleatorio) {
+    if (chute === numeroAleatorio) {
+        break;
+        
+    }
+    if(chute > numeroAleatorio) {
+        chute = prompt("O número que você digitou é menor que o número aleatorio,tente novamente")
+        tentativas++;
+    } else {
+        chute = prompt("O número que você digitou é menor que o número aleatorio,tente novamente")
+        tentativas++;
+    }
+}
